412ff5e2571a0b4bc662f945f240adcb0c9faf46
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-12 21:26:55 +0400

    Added camera + Start/Stop button ;

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from kivy.app import App
+from kivy.core.camera import Camera
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-from FileSharer import FileSharer
+# from FileSharer import FileSharer
 
 Builder.load_file("frontend.kv")
 
 
 class CameraScreen(Screen):
     def start(self):
-        pass
+        self.ids.camera.play = True
+        self.ids.camera_button.text = 'Stop Camera'
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.camera_button.text = 'Start Camera'
+        self.ids.camera.texture = None
 
     def capture(self):
         pass
@@ -28,6 +33,9 @@ class RootWidget(ScreenManager):
 class MainApp(App):
 
     def build(self):
+        # cam = Camera(play=True)
+        # setting the resolution when play is True causes the crash
+        # cam.resolution = (320, 240)
         return RootWidget()
 
 
